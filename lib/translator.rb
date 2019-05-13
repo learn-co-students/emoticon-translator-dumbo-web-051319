010cdaf68573651df4f0c4fd19a103d4b442b9f1
Joe Jamsky <cba1067950@yahoo.com>
2019-05-13 22:20:51 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,28 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
+#load_library
+  #accepts one argument, the file path
+  #return value
+    #returns a hash
+    #has two keys, 'get_meaning' and 'get_emoticon' (FAILED - 1)
+    #the keys 'get_meaning' and 'get_emoticon' point to inner hashes (FAILED - 2)
+    #the keys inside the 'get_meaning' hash are the Japanese emoticons (FAILED -3)
+    #the emoticon keys inside the 'get_meaning' hash point to their meanings (FAILED - 4)
+    #the keys inside the 'get_emoticon' hash are the English emoticons (FAILED -5)
+    #the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents (FAILED - 6)
+
+def load_library(path)
   # code goes here
+  data = YAML.load_file(path)
+  
+  YAML.dump("foo")
+    { :a => 'b'}.to_yaml
+  #data.map do |english, japanese|
+  #  binding.pry
+  #end
+  binding.pry
 end
 
 def get_japanese_emoticon
